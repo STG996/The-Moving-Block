2e4a95f815903fe930529bcb8b53ffe68ab5bf1d
STG996 <72745742+STG996@users.noreply.github.com>
2020-10-23 12:59:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -63,13 +63,6 @@ class Obstacle(pygame.sprite.Sprite):
         self.image.fill(color)
         self.rect = self.image.get_rect(topleft = (pos_x, pos_y))
 
-# class Key(pygame.sprite.Sprite):
-#     def __init__(self, width, height, pos_x, pos_y, color):
-#         super().__init__()
-#         self.image = pygame.Surface([width, height])
-#         self.image.fill(color)
-#         self.rect = self.image.get_rect(topleft = (pos_x, pos_y))
-
 pygame.init()
 clock = pygame.time.Clock()
 run = True
